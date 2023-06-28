76c653de9662a9cf35ef8ab67cae218f0b491eda
Leonel Menci <minterger@gmail.com>
2023-06-28 02:17:42 -0300

    feat(Main.vue): add support username skin

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,4 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const imagen = ref();
+
+async function buscarUUID(nombreJugador) {
+  try {
+    const response = await fetch(
+      `https://playerdb.co/api/player/minecraft/${nombreJugador}`
+    );
+    const data = await response.json();
+
+    const id = data.data.player.raw_id;
+
+    imagen.value = `https://crafatar.com/renders/body/${id}`;
+
+    if (response.ok) {
+      console.log(imagen.value);
+    } else {
+      console.log("No se encontró el jugador");
+    }
+  } catch (error) {
+    console.error("Error al buscar el UUID del jugador:", error);
+    imagen.value =
+      "https://crafatar.com/renders/body/069a79f444e94726a5befca90e38aaf5";
+  }
+}
+
+buscarUUID("Notch");
+</script>
 
 <template>
   <div
@@ -13,10 +42,6 @@
         para que te diviertas
       </p>
     </div>
-    <img
-      src="https://mskins.net/uploads/preview3d/42/4f/Kasidiarhs_350.png"
-      class="w-52 m-20 hidden md:block"
-      alt=""
-    />
+    <img :src="imagen" class="w-52 m-20 hidden md:block" alt="" />
   </div>
 </template>
