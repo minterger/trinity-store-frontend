e58445c72556f5773403350d1d4be4badef8c8fa
Leonel Menci <minterger@gmail.com>
2023-06-28 17:08:19 -0300

    feat(DiscordButton.vue): add scale and transition

--- src/components/DisordButton.vue
+++ src/components/DisordButton.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    class="rounded-full w-14 h-14 p-1 flex items-center justify-center bg-slate-700 bg-opacity-30 backdrop-blur"
+    class="rounded-full w-14 h-14 p-1 flex items-center justify-center bg-slate-700 bg-opacity-30 backdrop-blur hover:scale-110 transition-transform"
     href="http://discord.trinitymc.com.ar"
     target="_blank"
     rel="noopener noreferrer"
