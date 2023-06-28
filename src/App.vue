dd313d2e9fa8f3a8c3e765881ee08d54318311ca
Leonel Menci <minterger@gmail.com>
2023-06-28 12:37:41 -0300

    feat(discord): add discord fixed button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import DisordButton from "./components/DisordButton.vue";
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import Footer from "./components/Footer.vue";
     <Navbar />
     <RouterView />
     <Footer />
+    <DisordButton class="fixed bottom-8 right-8" />
   </div>
 </template>
---
+++ src/components/DisordButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <a
+    class="rounded-full w-14 h-14 p-1 flex items-center justify-center bg-slate-700 bg-opacity-30 backdrop-blur"
+    href="http://discord.trinitymc.com.ar"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img src="../assets/discord.png" alt="" />
+  </a>
+</template>
