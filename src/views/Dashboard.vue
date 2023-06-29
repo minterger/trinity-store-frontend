b80e32b1e4665f6366560d2eb9955853adefc33b
Leonel Menci <minterger@gmail.com>
2023-06-29 11:59:14 -0300

    feat(dashboard): possible dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,1 +1,21 @@
-<template>Dashboard</template>
+<template>
+  <div class="max-w-screen-lg px-2 mx-auto flex gap-4">
+    <nav
+      class="max-w-xs w-full rounded-md my-4 min-h-screen bg-slate-700 bg-opacity-30"
+    >
+      <ul>
+        <li>Cuenta</li>
+        <li>Historial</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+    <aside
+      class="w-full rounded-md my-4 min-h-screen bg-slate-700 bg-opacity-30"
+    >
+      asd
+    </aside>
+  </div>
+</template>
