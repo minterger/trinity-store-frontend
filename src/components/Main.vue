bb6bb1f875076e0c60c601b659c46246aaebc77e
Leonel Menci <minterger@gmail.com>
2023-07-05 15:10:38 -0300

    fix(img)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,21 +4,20 @@ import { useAuthStore } from "../stores/authStore";
 
 const authStore = useAuthStore();
 
-const imagen = ref(
-  "https://crafatar.com/renders/body/a0012be9cede48a38ae366c502192cb8?size=4&default=MHF_Steve&overlay"
-);
+const imagen = ref("");
 
-watch(
-  () => authStore.user.uuid,
-  () => {
-    if (!!authStore.user.uuid) {
-      imagen.value = `https://crafatar.com/renders/body/${authStore.user.uuid}?size=4&default=MHF_Steve&overlay`;
-    } else {
-      imagen.value =
-        "https://crafatar.com/renders/body/a0012be9cede48a38ae366c502192cb8?size=4&default=MHF_Steve&overlay";
-    }
+const traerImg = () => {
+  if (!!authStore.user.uuid) {
+    imagen.value = `https://crafatar.com/renders/body/${authStore.user.uuid}?size=4&default=MHF_Steve&overlay`;
+  } else {
+    imagen.value =
+      "https://crafatar.com/renders/body/a0012be9cede48a38ae366c502192cb8?size=4&default=MHF_Steve&overlay";
   }
-);
+};
+
+traerImg();
+
+watch(() => authStore.user.uuid, traerImg);
 </script>
 
 <template>
